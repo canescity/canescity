9e0acce09968f0291d1eccfb0fb645e2ef48750a
canescity <msf.01@gmx.com>
2024-11-20 21:56:58 -0300

    Create artwithstars.py

---
+++ arts/artwithstars.py
@@ -0,0 +1,18 @@
+# This is uncolored art. Use the color code to decorate the art as shown in example.py
+
+# Color Codes
+'''
+Copy these codes and paste it before the text for the color your wish
+① = grey
+② = red
+③ = green
+④ = yellow
+⑤ = blue
+⑥ = magenta
+⑦ = cyan
+⑧ = white
+⑨ = blink
+⑩ = remove blink
+® = random
+'''
+'''

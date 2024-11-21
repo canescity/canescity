8ab085759e1cf6ffe717a7ab879dc76b5160ca49
canescity <msf.01@gmx.com>
2024-11-20 23:16:41 -0300

    Update example.py

--- arts/example.py
+++ arts/example.py
@@ -1,3 +1,7 @@
+# This is an example of adding colors with color codes. Add colors you wish in 'art.py' and make sure to make ```artFile = art.py```  in config.py to use art.py
+
+mainArt = \
+'''
 dmitry -winsepo example.txt example.com
 D7eepmagic Information Gathering Tool
 "There be some deep magic going on"

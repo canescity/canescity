e24deff076a83dc5fcaf61472881d8d2caae77f7
canescity <msf.01@gmx.com>
2024-11-20 23:21:03 -0300

    Create example.py

---
+++ arts/example.py
@@ -0,0 +1,340 @@
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+
+# This is an example of adding col>
+
+mainArt = \
+'''
+
+⑤dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"⑤
+
+③Writing output to 'Mega Sena'③
+
+⑤HostIP:⑤③93.10:30 AM③
+⑤HostName:⑤③mistisuki.com③
+
+⑤ESPAÇO DA SORTE em SÃO PAULO, SP⑤
+③----------------------------------③
+⑤Concurso 2798⑤ ③(19/11/2024③)
+
+③03'09'18'54'59'60③ ⑤R$ 18.000.000,00⑤
+'''

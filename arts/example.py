d47b247ff3b3f361e9713455e0b01f1d4cf3dc3a
canescity <msf.01@gmx.com>
2024-11-20 23:13:37 -0300

    Create example.py

---
+++ arts/example.py
@@ -0,0 +1,451 @@
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+dmitry -winsepo example.txt example.com
+D7eepmagic Information Gathering Tool
+"There be some deep magic going on"
+
+Writing output to 'Mega Sena'
+
+HostIP:93.10:30 AM
+HostName:mistisuki.com
+
+ESPAÇO DA SORTE em SÃO PAULO, SP
+----------------------------------
+Concurso 2798 (19/11/2024)
+
+03'09'18'54'59'60 R$ 18.000.000,00
+
+'''

9ad08485e30bceafcf58ac0ce7137175a6d14349
canescity <msf.01@gmx.com>
2024-12-02 04:43:08 -0300

    Update and rename example to example.py

--- arts/example
+++
@@ -1,5 +0,0 @@
-# This is an example of adding colors with color codes. Add colors you wish in 'art.py' and make sure to make ```artFile = art.py```  in config.py to use art.py
-
-mainArt = \
-'''
-'''
---
+++ arts/example.py
@@ -0,0 +1,188 @@
+# This is an example of adding colors with color codes. Add colors you wish in 'art.py' and make sure to make ```artFile = art.py```  in config.py to use art.py
+
+mainArt = \
+'''
+③
+__  __       _ _   _       __  __
+|  \/  |_   _| | |_(_)     |  \/  | __ _  ___
+| |\/| | | | | | __| |_____| |\/| |/ _` |/ __|
+| |  | | |_| | | |_| |_____| |  | | (_| | (__
+|_|  |_|\__,_|_|\__|_|     |_|  |_|\__,_|\___|
+③
+⑥
+[-] Melhor Truque de Meatre [Hello Hackers]
+[+] Truque dev Psicopatia  Multi-Mac Chknet
+[=] Multi-Mac Tool Version : 2.0
+
+[x] 1) List valid 6 number valid
+[x] 2) Generate enumerate (generate cc list)
+[x] 3) Mac validator
+[x] 4) Generate Multi-Mac Mega Sena
+
+[^] Please Enter an option :  1
+[&] You selected first option !
+⑥
+③
+[-] Amount : 6 Numeros
+[-] Aposta : 11 24 26 27 37 55
+[-] Concurso : 2802
+[-] aconteceu : 02/12/24
+[-] Nome : Wallex Guimarães
+[-] Address : 45860-000 Canavieiras
+[-] Country : Brazil
+[-] Money Range : $66.000
+[-] Moeda : Real Brasileiro
+[-] UF : Bahia
+[-] Expiry : 12/2024
+③
+⑥
+[-] Melhor Truque de Meatre [Hello Hackers]
+[+] Truque dev Psicopatia  Multi-Mac Chknet
+[=] Multi-Mac Tool Version : 2.0
+
+[x] 1) List valid 6 number valid
+[x] 2) Generate enumerate (generate cc list)
+[x] 3) Mac validator
+[x] 4) Generate Multi-Mac Mega Sena
+
+[^] Please Enter an option :  1
+[&] You selected first option !
+
+[-] Pin : 2203
+============================
+[*] Script by @Wally
+⑥
+③
+[-] Amount : 6 Numeros
+[-] Aposta : 14 27 43 44 46 48  
+[-] Concurso : 2802
+[-] aconteceu : 02/12/24
+[-] Nome : Wallex Guimarães
+[-] Address : 45860-000 Canavieiras
+[-] Country : Brazil
+[-] Money Range : $66.000
+[-] Moeda : Real Brasileiro
+[-] UF : Bahia
+[-] Expiry : 12/2024
+③
+⑥
+[-] Melhor Truque de Meatre [Hello Hackers]
+[+] Truque dev Psicopatia  Multi-Mac Chknet
+[=] Multi-Mac Tool Version : 2.0
+
+[x] 1) List valid 6 number valid
+[x] 2) Generate enumerate (generate cc list)
+[x] 3) Mac validator
+[x] 4) Generate Multi-Mac Mega Sena
+
+[^] Please Enter an option :  1
+[&] You selected first option !
+
+[-] Pin : 2203
+============================
+[*] Script by @Wally
+⑥
+③
+[-] Amount : 6 Numeros
+[-] Aposta : 12 21 50 52 53 58  
+[-] Concurso : 2802
+[-] aconteceu : 02/12/24
+[-] Nome : Wallex Guimarães
+[-] Address : 45860-000 Canavieiras
+[-] Country : Brazil
+[-] Money Range : $66.000
+[-] Moeda : Real Brasileiro
+[-] UF : Bahia
+[-] Expiry : 12/2024
+③
+⑥
+[-] Melhor Truque de Meatre [Hello Hackers]
+[+] Truque dev Psicopatia  Multi-Mac Chknet
+[=] Multi-Mac Tool Version : 2.0
+
+[x] 1) List valid 6 number valid
+[x] 2) Generate enumerate (generate cc list)
+[x] 3) Mac validator
+[x] 4) Generate Multi-Mac Mega Sena
+
+[^] Please Enter an option :  1
+[&] You selected first option !
+
+[-] Pin : 2203
+============================
+[*] Script by @Wally
+⑥
+③
+[-] Amount : 6 Numeros
+[-] Aposta :  04 16 27 30 35 57  
+[-] Concurso : 2802
+[-] aconteceu : 02/12/24
+[-] Nome : Wallex Guimarães
+[-] Address : 45860-000 Canavieiras
+[-] Country : Brazil
+[-] Money Range : $66.000
+[-] Moeda : Real Brasileiro
+[-] UF : Bahia
+[-] Expiry : 12/2024
+③
+⑥
+[-] Melhor Truque de Meatre [Hello Hackers]
+[+] Truque dev Psicopatia  Multi-Mac Chknet
+[=] Multi-Mac Tool Version : 2.0
+
+[x] 1) List valid 6 number valid
+[x] 2) Generate enumerate (generate cc list)
+[x] 3) Mac validator
+[x] 4) Generate Multi-Mac Mega Sena
+
+[^] Please Enter an option :  1
+[&] You selected first option !
+
+[-] Pin : 2203
+============================
+[*] Script by @Wally
+⑥
+③
+[-] Amount : 6 Numeros
+[-] Aposta : 01 06 08 46 53 59    
+[-] Concurso : 2802
+[-] aconteceu : 02/12/24
+[-] Nome : Wallex Guimarães
+[-] Address : 45860-000 Canavieiras
+[-] Country : Brazil
+[-] Money Range : $66.000
+[-] Moeda : Real Brasileiro
+[-] UF : Bahia
+[-] Expiry : 12/2024
+③
+⑥
+[-] Melhor Truque de Meatre [Hello Hackers]
+[+] Truque dev Psicopatia  Multi-Mac Chknet
+[=] Multi-Mac Tool Version : 2.0
+
+[x] 1) List valid 6 number valid
+[x] 2) Generate enumerate (generate cc list)
+[x] 3) Mac validator
+[x] 4) Generate Multi-Mac Mega Sena
+
+[^] Please Enter an option :  1
+[&] You selected first option !
+
+[-] Pin : 2203
+============================
+[*] Script by @Wally
+⑥
+③
+[-] Amount : 6 Numeros
+[-] Aposta : 07 12 26 47 49 58     
+[-] Concurso : 2802
+[-] aconteceu : 02/12/24
+[-] Nome : Wallex Guimarães
+[-] Address : 45860-000 Canavieiras
+[-] Country : Brazil
+[-] Money Range : $66.000
+[-] Moeda : Real Brasileiro
+[-] UF : Bahia
+[-] Expiry : 12/2024
+③
+'''
